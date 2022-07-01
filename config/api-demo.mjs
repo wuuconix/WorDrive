8834bd02a931cc8eefc6ee438a27c3ed22bf7953
wuuconix <1521900139@qq.com>
2022-07-01 19:34:10 +0800

    add config demo

---
+++ config/api-demo.mjs
@@ -0,0 +1,2 @@
+/* 根据实际情况改变变量 之后把文件名改为api.mjs */
+export const mongoURI = "mongodb://user:pass@localhost/db"
---
+++ config/web-demo.js
@@ -0,0 +1,2 @@
+/* 根据实际情况改变变量 之后把文件名改为web.js */
+export const apiBaseURI = "http://wordapi.localhost/"
